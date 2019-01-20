78801b919ab9c45385d53c5c24899626c8fc1753
BazylPL <piotrbaz@gmail.com>
2019-01-20 13:35:41 +0100

    md5, sha256, sha512 hashes on the fly

--- src/app/password/model/Password.ts
+++ src/app/password/model/Password.ts
@@ -1,4 +1,8 @@
 import {Strength} from './Strength';
+import * as md5 from 'js-md5';
+import {sha256} from 'js-sha256';
+import {sha512} from 'js-sha512';
+
 
 export class Password {
     _plain: string;
@@ -15,6 +19,11 @@ export class Password {
 
     set plain(newPlain: string) {
         this._plain = newPlain;
+
+        // regenerate and set hashed values
+        this._md5 = md5(this._plain);
+        this._sha256 = sha256(this._plain);
+        this._sha512 = sha512(this._plain);
     }
 
     // Getters
@@ -24,15 +33,15 @@ export class Password {
     }
 
     get md5(): string {
-        return this._md5; // use MD5 function here?
+        return this._md5;
     }
 
     get sha256(): string {
-        return this._sha256; // use sha256 function here?
+        return this._sha256;
     }
 
     get sha512(): string {
-        return this._sha512; // use sha512 function here?
+        return this._sha512;
     }
 
     get strength(): Strength {
